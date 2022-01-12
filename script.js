d8d05d1b9003e14f841075ffc22b33cec2c9945c
karamazovosky <karamazovosky@gmail.com>
2022-01-12 14:22:38 +0800

    Add computer

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hi!')
+function computerPlay (type) {
+    x = Math.floor(Math.random()*3);
+    if (x === 0) {
+        type = 'Rock';
+    }else if(x===1){
+        type ='Paper';
+    }else {
+        type ='Scissor';
+    }
+    return(type);
+}
+
+console.log(computerPlay());
