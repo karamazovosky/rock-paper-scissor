af4542496f104720eec48b607eed156c079e2d94
karamazovosky <karamazovosky@gmail.com>
2022-01-12 18:18:46 +0800

    Add playround and game function

--- script.js
+++ script.js
@@ -8,16 +8,12 @@ function playerPlay() {
     let check = true;
     while (check) {
         let hand = prompt('Select your hand (Rock/Paper/Scissors) :');
-        console.log(hand);
         hand = hand.toLowerCase();
         if (hand==='rock') {
-            check = false;
             return hand = 0;
         }else if (hand==='paper') {
-            check = false;
             return hand = 1;
         }else if (hand==='scissors') {
-            check = false;
             return hand = 2;
         }else {
             alert('Please Input the Correct Play')
@@ -26,34 +22,60 @@ function playerPlay() {
 }
 
 function playRound (x = computerPlay(), y = playerPlay()) {
-    let result
+    let result;
     //rock > scissors
-    if (x === 0 && y === 2 ){
-        return (result = "You Lost ! Rock Beats Scissors!")
+    if (x === 0 && y === 2){
+        console.log("You Lost ! Rock Beats Scissors!");
+        return result = 0;
     } else if (x === 2 && y === 0) {
-        return (result = "You Win ! Rock Beats Scissors !")
+        console.log("You Win ! Rock Beats Scissors!");
+        return result = 1;
     }
     //paper > rock
     if (x === 1 && y === 0) {
-        return (result = "You Lost ! Paper Beats Rocks!")
-    }else if(x === 1 && y === 0) {
-        return (result = "You Win! Paper Beats Rocks!")
+        console.log("You Lost ! Paper Beats Rocks!");
+        return result = 0;
+    }else if(x === 0 && y === 1) {
+        console.log("You Win ! Paper Beats Rocks!");
+        return result = 1;
     }
-
     //scissors > paper
     if (x === 2 && y ===1) {
-        return (result = "You Lost ! Scissors Beats Paper!");
+        console.log("You Lost ! Scissors Beats Paper!");
+        return result = 0;
     }else if(x === 1 && y ===2) {
-        return (result = "You Lost ! Scissors Beats Paper!");
+        console.log("You Win ! Scissors Beats Paper!");
+        return result = 1;
     }
     //draw
-    if (x === y === 0) {
-        return (result("Its a Draw! Both Side Picked Rock!"));
-    }else if (x === y === 1) {
-        return (result("Its a Draw! Both Side Picked Paper!"));
-    }else if(x === y === 2) {
-        return (result("Its a Draw! Both Side Picked Scissors!"));
+    if (x === y && x === 0) {
+        console.log("Its a Draw! Both Side Picked Rock!");
+        return result = 2;
+    }else if (x === y && x === 1) {
+        console.log("Its a Draw! Both Side Picked Paper!");
+        return result = 2;
+    }else if(x === y && x === 2) {
+        console.log("Its a Draw! Both Side Picked Scissors!");
+        return result = 2;
     }
 }
-console.log(playRound());
 
+function game() {
+    let winlose = [];
+    for (let i = 0; i<5; i++) {
+        let score = playRound();
+        let status;
+        if (score === 0) {
+            status = "Lost";
+        }else if (score === 1) {
+            status = "Win";
+        }else if (score === 2) {
+            status = "Draw"
+        }
+        winlose.push(status);
+    }
+    for (let j = 0; j<5; j++) {
+        console.log(winlose[j]);
+    }
+}
+game()
